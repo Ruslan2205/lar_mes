2095a722613db42abdfec717f1bea717fbfec02a
Ruslan2205 <54228532+Ruslan2205@users.noreply.github.com>
2026-01-14 01:19:23 +0300

    130126

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,39 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canRegister' => Features::enabled(Features::registration()),
+//    ]);
+//})->name('home');
+
+
+Route::get('/', [MyPlaceController::class, 'index'])->name('home');
+
+
+Route::get('/my_page', function (){
+    return 'This is my page';
+})->name('my_page');
+
+
+
+Route::get('/about_us', function (){
+    return 'This is about_us page';
+})->name('about_us');
+
+Route::get('/nomenclature', function (){
+    return 'This is nomenclature page';
+})->name('nomenclature');
+
+Route::get('/specification', function (){
+    return 'This is specification page';
+})->name('specification');
+
+
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
